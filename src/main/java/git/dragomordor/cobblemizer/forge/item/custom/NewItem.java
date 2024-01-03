c141fcac2dba2f3da9467e231b1293e5229f23bb
Dragonofmordor <ruanvandenberg12@gmail.com>
2024-01-03 18:28:11 +0200

    Version 2.0.0

---
+++ src/main/java/git/dragomordor/cobblemizer/forge/item/custom/NewItem.java
@@ -0,0 +1,15 @@
+package git.dragomordor.cobblemizer.forge.item.custom;
+
+public class NewItem {
+
+    // TODO items for following features
+        /*
+        pokemon.setAbility();
+        pokemon.setAspects();
+        pokemon.setFeatures();
+        pokemon.setForm();
+        pokemon.setState();
+        pokemon.setTeraType();
+         */
+
+}
